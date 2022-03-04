d91d78fa46d53dc315f462f23581c8d252699748
CHIDANANDAM-BHATLU <bhatlu25@gmail.com>
2022-03-04 22:21:34 +0530

    Add project files.

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using EmployeeService3.Models;
+
+namespace EmployeeService3.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        public IEnumerable<Employee> GetEmployees()
+        {
+            using(EmployeeEntities employeeEntities = new EmployeeEntities())
+            {
+                return employeeEntities.Employees.ToList();
+            }
+        }
+
+        public Employee GetEmployee(int id)
+        {
+            using(EmployeeEntities employeeEntities = new EmployeeEntities())
+            {
+                return employeeEntities.Employees.FirstOrDefault(e => e.ID == id);
+            }
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EmployeeService3.WebApiApplication" Language="C#" %>
